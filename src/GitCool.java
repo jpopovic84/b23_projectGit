8e85dff80ec257632e0c9a4d07d6286ec916f831
jpopovic84 <jpopovic984@gmail.com>
2021-07-19 16:42:22 -0400

    added GitHub account

--- src/GitCool.java
+++ src/GitCool.java
@@ -3,4 +3,8 @@ public class GitCool {
     // b23 ios very special number
     // b23 is making sense now
 
+
+    // We have added our GitHub account under IntelliJ
+    // make sure to click it and apply
+
 }
