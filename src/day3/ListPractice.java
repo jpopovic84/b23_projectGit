e987bf5f6680ceb3a4af74f94d15ee9185ce893a
jpopovic84 <jpopovic984@gmail.com>
2021-07-29 14:33:54 -0400

    added new items

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -1,10 +1,16 @@
 package day3;
 
+import java.util.ArrayList;
+
 public class ListPractice {
 
     public static void main(String[] args) {
 
         //Hello
+        // What's new today?
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(123);
+
 
 
     }
